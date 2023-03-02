d8ad0d6634dfecc50c46b279e8e7ea84c77aafe0
Ed Jee <edjee96@gmail.com>
2023-03-02 01:53:17 -0600

    added na rm

--- R/estimate-aggregations.R
+++ R/estimate-aggregations.R
@@ -1,5 +1,5 @@
 
 calculate_event_study = function(att_pr_df, y_var){
-    manual_es = att_pr_df[, .(wt = pr/sum(pr), att_g_t = get(y_var)), event.time][, .(estimate = sum(wt*att_g_t)), event.time ]
+    manual_es = att_pr_df[, .(wt = pr/sum(pr), att_g_t = get(y_var)), event.time][, .(estimate = sum(wt*att_g_t, na.rm = TRUE)), event.time ]
     return(manual_es)
 }
