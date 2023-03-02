09572d2d640c0d6a2796ffb5478e98abe4afe1fb
Ed Jee <edjee96@gmail.com>
2023-03-02 02:04:48 -0600

    added influence matrix fixing

--- R/estimate-did.R
+++ R/estimate-did.R
@@ -158,7 +158,13 @@ estimate_event_study = function(att_df,
         att_df[, max_et := max(event.time), group]
         att_df = att_df[max_et >= balance_e & event.time <= balance_e]
     }
-    att_df[!is.na(get(y_var)), wt := pr/sum(pr), .(event.time, treated)]
+    # Dealing with NA ATTs
+    not_na_atts = att_df[, !is.na(get(y_var))]
+    att_df = att_df[not_na_atts]
+    inf_matrix = inf_matrix[, not_na_atts]
+
+
+    att_df[, wt := pr/sum(pr), .(event.time, treated)]
     agg_pr = att_df[, unique(pr), .(event.time)][, V1]
 
     es_df = calculate_event_study(att_df, y_var = y_var)
@@ -207,4 +213,89 @@ estimate_event_study = function(att_df,
     es_df[, std.error := et_se]
     setorder(es_df, event.time)
     return(es_df)
+}
+
+#' Estimate Group Average 
+#'
+#' Given a data.table of ATTs and an influence matrix, create group level estimates.
+#' 
+#' @param att_df Data.table of ATTs
+#' @param inf_matrix Influence function matrix, N individuals by K ATTs
+#' @param balance_e Balance event study for e time periods
+#' @param y_var Column name in att_df
+#' @param biter Bootstrap draws for inference
+#' @param n_cores Number of cores for parallal processing
+#' @param prop_score_known Propensity score known or to be estimated
+#' @param group_vector Nx1 vector of group IDs
+#' @param cluster_id Nx1 vector of cluster IDs
+#' @export 
+estimate_group_average = function(att_df, 
+                                  inf_matrix,
+                                  balance_e = NULL, 
+                                  y_var = att_g_t, 
+                                  biter = 1000,
+                                  n_cores = 8,
+                                  cluster_id = NULL,
+                                  group_vector
+                                  ) {
+    # att_df = manual_did
+    # biter = 100
+    # n_cores = 4
+    # prop_score_known = FALSE
+    # group_vector = summ_indiv_dt[, G]
+    # balance_e = NULL
+    # y_var = "att_g_t"
+
+    # selective.se.inner <- lapply(glist, function(g) {
+    #   whichg <- which( (group == g) & (g <= t) & (t<= (group + max_e)))  ### added last condition to allow for limit on longest period included in att
+    #   inf.func.g <- as.numeric(get_agg_inf_func(att=att,
+    #                                             inffunc1=inffunc1,
+    #                                             whichones=whichg,
+    #                                             weights.agg=pg[whichg]/sum(pg[whichg]),
+    #                                             wif=NULL))
+    #   se.g <- getSE(inf.func.g, dp)
+    #   list(inf.func=inf.func.g, se=se.g)
+    # })
+ 
+
+    if (!is.null(balance_e)) {
+        att_df[, max_et := max(event.time), group]
+        att_df = att_df[max_et >= balance_e & event.time <= balance_e]
+    }
+
+    group_dt = calculate_group_average(att_df, y_var = y_var)
+    groups = unique(att_df$group)
+
+    # pr group
+    pg = sapply(groups, function(g) mean(group_vector == g))
+    # pr group by group
+    pgg = pg
+    # now matches number og atts
+    pg = pg[match(att_df$group, groups)]
+
+
+
+
+    whichones = map(
+        groups,
+        ~which(att_df[, group == .x])
+    )
+
+    inf_func_group = map(
+        whichones,
+        ~get_agg_inf_func(
+            att = att_pr_df,
+            inffunc1 = inf_matrix, 
+            whichones = .x,
+            weights.agg = pg[.x]/sum(pg[.x]) , 
+            wif = NULL
+        )
+    )
+
+    group_se = map_dbl(
+        inf_func_group,
+        ~calculate_se(.x, biter = biter, n_cores = n_cores, cluster_id = cluster_id)
+    )
+    group_dt[, std.error := c(NA, group_se)]
+    return(group_dt)
 }
